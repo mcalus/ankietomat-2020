d91d17b639c0d187d96cb0e604fddc2490be1822
mcalus <jam120@wp.pl>
2021-01-04 15:22:42 +0100

    Added config file

--- server.js
+++ server.js
@@ -2,12 +2,16 @@ const express = require('express')
 const session = require('express-session');
 const passport = require('passport');
 const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
 const app = express()
 
 // Required to read cookies
 app.use(cookieParser());
+
+app.use(bodyParser.urlencoded({ extended: true }))
+
 passport.serializeUser(function(user, next) {
     // Serialize the user in the session
     next(null, user);
@@ -35,7 +39,8 @@ app.listen(3000, () => {
  
     if(typeof(process.argv[2]) === 'undefined' || process.argv[2] !== 'silent')
         console.log('Server started and is listening on port 3000')
-
+    
+    app.locals.config = require('./config.json')
     app.locals.logged = false
     app.locals.siteUrl = ""
     // app.locals({ 
@@ -111,4 +116,81 @@ app.get('/survey/stats', (request, respond) => {
 
 })
 
-require('./db.js')(app);
+
+app.get('/testDB', (request, respond) => {
+
+    var url = app.locals.config.db.url
+    
+    mongoose.connect(url, {useNewUrlParser: true, useUnifiedTopology: true});
+
+    // const Cat = mongoose.model('Cat', { name: String });
+
+    // const kitty = new Cat({ name: 'Zildjian' });
+    // kitty.save().then(() => console.log('meow'));
+    
+    respond.render('pages/testDB',  {
+        message: message,
+        collections: collections,
+        collection: coll,
+        rows: rows
+    })
+
+    return true;
+
+    var MongoClient = require('mongodb').MongoClient;
+
+    MongoClient.connect(url, function(err, db) {
+        if (err) throw err;
+        console.log("Database connected!");
+        var message = '';
+        var collections = '';
+        var rows = '';
+        var coll = 'customers3';
+
+        var dbo = db.db(mydb);
+        var rowInsert = { name: "Company Inc", address: "Highway 37" };
+
+        if(!dbo.collection(coll)) {
+            dbo.createCollection(coll, function(err, res) {
+                if (err) throw err;
+                console.log("Collection created!");
+                globalThis.message += "Collection created! <br/>";
+            });
+        }
+
+        // dbo.collection(coll).insertOne(rowInsert, function(err, res) {
+        //     if (err) throw err;
+        //     console.log("1 document inserted");
+        //     message += "1 document inserted: "+rowInsert+"<br />";
+        // });
+
+        dbo.collection(coll).findOne({}, function(err, result) {
+            if (err) throw err;
+            if(result) {
+                console.log(result.name);
+                message += "Row: "+result.name;
+            }
+        });
+
+        dbo.listCollections().toArray(function(err, collInfos) {
+            // console.log(JSON.stringify(collInfos));
+            collections = JSON.stringify(collInfos);
+        });
+
+        dbo.collection(coll).find().toArray(function(err, docs) {
+            // console.log(JSON.stringify(docs));
+            rows = JSON.stringify(docs);
+        });
+        
+        db.close();
+
+    });
+
+    respond.render('pages/testDB',  {
+        message: message,
+        collections: collections,
+        collection: coll,
+        rows: rows
+    })
+
+})
